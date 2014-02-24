cd7f94705e193eb7cc60a7ff0351b87939441903
Aaron <amcintyre2@mail.csuchico.edu>
2014-02-23 19:02:03 -0800

    First Commit

---
+++ app/src/main/java/com/example/app/SearchActivity.java
@@ -0,0 +1,29 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by ShaDynastys on 2/23/14.
+ */
+public class SearchActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_layout);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) findViewById(R.id.search_box);
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        // Show the Up button in the action bar.
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
